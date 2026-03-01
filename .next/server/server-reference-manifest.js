72a9afb60e48a1f35207824546c389634c5761f3
haron nyumu <nyumuharon@gmail.com>
2026-03-01 19:33:54 +0300

    chore: include next.config.mjs and untracked files for vercel deployment

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"0FCjfsR0ffaHehLykPYJNcVfbWH5JkT/i453Mqw8f7Y=\"}"
---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'i.pravatar.cc' },
+      { protocol: 'https', hostname: 'commons.wikimedia.org' },
+    ],
+  },
+};
+
+export default nextConfig;
