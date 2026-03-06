03f3605dfa367159345823b602eadc21963a1207
haron nyumu <nyumuharon@gmail.com>
2026-03-06 10:17:20 +0300

    chore: enable Vercel image optimization, remove static export artifact

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'i.pravatar.cc' },
