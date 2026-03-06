673e83d41d0bb75458a653eb01df1535d9441ba9
haron nyumu <nyumuharon@gmail.com>
2026-03-06 10:55:58 +0300

    chore: add phone number to footer credit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -46,7 +46,7 @@ export default function Footer() {
 
             </div>
             <div className="footer-bottom">
-                <p>© 2026 Mary Motors. All rights reserved. | Designed by haron</p>
+                <p>© 2026 Mary Motors. All rights reserved. | Designed by haron +254702864025</p>
             </div>
         </footer>
     );
